4c61b811c690e9205907c42312fa05776bea8914
physical-100 <jcc140202@naver.com>
2023-12-02 20:38:50 +0900

    gram으로 수정 추가

--- app/src/main/java/com/example/mealplanb/fragment/SpecificFood_Fragment.kt
+++ app/src/main/java/com/example/mealplanb/fragment/SpecificFood_Fragment.kt
@@ -3,10 +3,15 @@ package com.example.mealplanb.fragment
 import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import com.example.mealplanb.MealData
 import com.example.mealplanb.databinding.FragmentSpecificFoodBinding
@@ -32,6 +37,7 @@ class SpecificFood_Fragment : BottomSheetDialogFragment()  {
     private val firebaseDatabase = FirebaseDatabase.getInstance()
     val currentTime = Calendar.getInstance().time
     val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    var isclickededitgram:Boolean= false
     lateinit var mealName:String
     val realTime = dateFormat.format(currentTime) //현재 시간 받아오는거
 
@@ -50,7 +56,7 @@ class SpecificFood_Fragment : BottomSheetDialogFragment()  {
             // detail에서 수정된 값을 받아옴
             modifyfooddata = arguments?.getParcelable<MealData>("modify food")
             if(modifyfooddata!=null){
-                //modifydata에서 받아오는건 여기로 저장
+                //modifydata에서 받아오는건 여기로 저현
                 specificfooddata = food(
                     modifyfooddata!!.foodname,
                     modifyfooddata!!.foodbrand,
@@ -102,7 +108,7 @@ class SpecificFood_Fragment : BottomSheetDialogFragment()  {
         val fatview=binding.fattextView2
         val kcalview=binding.kcaltextView
         val foodamountview=binding.edtgram
-        val perpersonbutton=binding.switch100g
+        val perpersonbutton=binding.editamountfood
 
 
         // specificfooddata로 수정
@@ -118,32 +124,121 @@ class SpecificFood_Fragment : BottomSheetDialogFragment()  {
         proteinview?.text=String.format("%.1f",proteinperg*foodamount)
         fatview?.text=String.format("%.1f",fatperg*foodamount)
         kcalview?.text=String.format("%.1f",kcalperg*foodamount)
-        perpersonbutton?.text=String.format("%.1f",foodamount)
+        perpersonbutton?.text="1인분 ("+String.format("%.1f",foodamount)+")"
+
         var editfoodamount=foodamount/specificfooddata!!.foodamount
 
+        binding.editgramfood.setOnClickListener {
+            isclickededitgram= true
+            binding.edtgram.text = null
+            foodamountview?.hint=String.format("%.1f",foodamount)
+        }
+        binding.editamountfood.setOnClickListener {
+            isclickededitgram= false
+            binding.edtgram.text = null
+            foodamountview?.hint=String.format("%.1f",foodamount/specificfooddata!!.foodamount)
+        }
 
 
 
         binding.plus.setOnClickListener{
+            if(isclickededitgram==true){
+                editfoodamount=foodamountview?.hint.toString().toDouble()+1
+                foodamountview?.hint=String.format("%.1f",editfoodamount)
+                carboview?.text=String.format("%.1f",carboperg*editfoodamount)
+                proteinview?.text=String.format("%.1f",proteinperg*editfoodamount)
+                fatview?.text=String.format("%.1f",fatperg*editfoodamount)
+                kcalview?.text=String.format("%.1f",kcalperg*editfoodamount)
+
+            }
+
+            else{
+
             editfoodamount=foodamountview?.hint.toString().toDouble()+0.5
             foodamountview?.hint=String.format("%.1f",editfoodamount)
             carboview?.text=String.format("%.1f",carboperg*editfoodamount*foodamount)
             proteinview?.text=String.format("%.1f",proteinperg*editfoodamount*foodamount)
             fatview?.text=String.format("%.1f",fatperg*editfoodamount*foodamount)
             kcalview?.text=String.format("%.1f",kcalperg*editfoodamount*foodamount)
+            }
         }
 
         binding.minus.setOnClickListener{
-            editfoodamount=foodamountview?.hint.toString().toDouble()-0.5
-            foodamountview?.hint=String.format("%.1f",editfoodamount)
-            carboview?.text=String.format("%.1f",carboperg*editfoodamount*foodamount)
-            proteinview?.text=String.format("%.1f",proteinperg*editfoodamount*foodamount)
-            fatview?.text=String.format("%.1f",fatperg*editfoodamount*foodamount)
-            kcalview?.text=String.format("%.1f",kcalperg*editfoodamount*foodamount)
+            if(isclickededitgram==true){
+                editfoodamount=foodamountview?.hint.toString().toDouble()-1
+                foodamountview?.hint=String.format("%.1f",editfoodamount)
+                carboview?.text=String.format("%.1f",carboperg*editfoodamount)
+                proteinview?.text=String.format("%.1f",proteinperg*editfoodamount)
+                fatview?.text=String.format("%.1f",fatperg*editfoodamount)
+                kcalview?.text=String.format("%.1f",kcalperg*editfoodamount)
+
+            }else {
+                editfoodamount = foodamountview?.hint.toString().toDouble() - 0.5
+                foodamountview?.hint = String.format("%.1f", editfoodamount)
+                carboview?.text = String.format("%.1f", carboperg * editfoodamount * foodamount)
+                proteinview?.text = String.format("%.1f", proteinperg * editfoodamount * foodamount)
+                fatview?.text = String.format("%.1f", fatperg * editfoodamount * foodamount)
+                kcalview?.text = String.format("%.1f", kcalperg * editfoodamount * foodamount)
+            }
         }
 
+        binding.edtgram.setOnEditorActionListener { _, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE ||
+                (event.action == KeyEvent.ACTION_DOWN && event.keyCode == KeyEvent.KEYCODE_ENTER)
+            ) {
+                // EditText의 키보드에서 "Done" 버튼이나 Enter 키를 눌렀을 때의 동작
+
+                // EditText의 값을 가져옴
+                val inputValue = binding.edtgram.text.toString()
+                // 가져온 값을 사용하여 원하는 작업 수행
+                // 예: 가져온 값이 빈 문자열이 아닌 경우
+                if (inputValue.isNotEmpty()) {
+                    val inputNumber = inputValue.toDoubleOrNull()
+                    // inputNumber를 사용하여 원하는 작업 수행
+                    // 예: Toast 메시지로 값 표시
+                    if (inputNumber != null) {
+                        // 변환에 성공한 경우
+                       // Toast.makeText(this, "입력된 값: $inputNumber", Toast.LENGTH_SHORT).show()
+                        if(isclickededitgram==true){
+                            editfoodamount=inputNumber
+                            foodamountview?.hint=String.format("%.1f",editfoodamount)
+                            carboview?.text=String.format("%.1f",carboperg*editfoodamount)
+                            proteinview?.text=String.format("%.1f",proteinperg*editfoodamount)
+                            fatview?.text=String.format("%.1f",fatperg*editfoodamount)
+                            kcalview?.text=String.format("%.1f",kcalperg*editfoodamount)
+
+                        }
+
+
+                    } else {
+                        // 변환에 실패한 경우
+                        //Toast.makeText(this, "올바른 숫자를 입력하세요.", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    // 빈 문자열인 경우
+
+                    //Toast.makeText(this, "값을 입력하세요.", Toast.LENGTH_SHORT).show()
+                }
+
+                // 이벤트 처리 완료를 알리기 위해 true 반환
+                true
+            } else {
+                // 이벤트 처리를 계속 진행하기 위해 false 반환
+                false
+            }
+        }
+
+
+
+
         binding.plusmeal.setOnClickListener{
-            saveDataToFirebase(editfoodamount) //데이터베이스 저장
+            if(isclickededitgram==true){
+                editfoodamount=foodamountview?.hint.toString().toDouble()/specificfooddata!!.foodamount
+                saveDataToFirebase(editfoodamount)
+            }
+            else {
+                saveDataToFirebase(editfoodamount) //데이터베이스 저장
+            }
             dismiss()
         }
 
@@ -202,7 +297,7 @@ class SpecificFood_Fragment : BottomSheetDialogFragment()  {
         super.onViewCreated(view, savedInstanceState)
 
         }
-    fun setOnNumberEnteredListener(listener: OnfoodEnteredListener) {
+    fun setOnNumberEnteredListener(listener: SpecificFood_Fragment.OnfoodEnteredListener) {
         this.foodaddlistener = listener
     }
     }
